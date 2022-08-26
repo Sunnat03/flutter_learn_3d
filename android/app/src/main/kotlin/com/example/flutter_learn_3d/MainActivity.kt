63b23717c693d0dbf85c9f3ad0fb3eb797a04ad8
Sunnat03 <sunnatmaxmayusupov02@gmail.com>
2022-08-26 18:12:58 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_learn_3d/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_learn_3d
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn_3d/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter 3D',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
